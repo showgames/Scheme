47950d9c9f218c2efcf215197bda997809d88fbf
Shohei Hayashi <2329shohei@gmail.com>
2015-12-06 21:05:03 +0900

    first commit

---
+++ insert.scm
@@ -0,0 +1,22 @@
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((eq? (car lat) old)
+	     (cons old
+		   (cons new (cdr lat))))
+	    (else (cons (car lat)
+			(insertR new old
+				 (cdr lat)))))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((eq? (car lat) old)
+	     (cons new lat))
+	    (else (cons (car lat)
+			(insertL new old
+				 (cdr lat)))))))))
