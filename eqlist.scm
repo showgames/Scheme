046b502e989242ed45eedcabc32f21ae12f21507
Shohei Hayashi <2329shohei@gmail.com>
2015-12-11 23:49:07 +0900

    first commit

---
+++ eqlist.scm
@@ -0,0 +1,24 @@
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+     ((and (number? a1) (number? a2))
+      (= a1 a2))
+     ((or (number? a1) (number? a2))
+      #f)
+     (else
+      (eq? a1 a2)))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1) (null? l2)) #t)
+     ((or (null? l1) (null? l2)) #f)
+     ((and (atom? (car l1))
+	   (atom? (car l2)))
+      (and (eqan? (car l1) (car l2))
+	   (eqlist? (cdr l1) (cdr l2))))
+     ((or (atom? (car l1) (car l2))) #f)
+     (else
+      (and (eqlist? (car l1) (car l2))
+	   (eqlist? (cdr l1) (cdr l2)))))))
+
